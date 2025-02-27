0e4fd06720e646ad6763fc6f37efca2eb6e291e5
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-02-27 10:42:27 +0100

    basic JS for Form with typewriter

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+function generateRecipe(event) {
+  event.preventDefault();
 
+  new Typewriter("#generatedRecipe", {
+    strings: "Here is your new recipe: ",
+    autoStart: true,
+  });
+}
+
+let formElement = document.querySelector("#recipeForm");
+
+formElement.addEventListener("submit", generateRecipe);
