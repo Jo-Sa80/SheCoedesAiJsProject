25807977baa7cc8d532c212e71682afb61a5886c
Jo-Sa80 <j.sandvoss80@gmail.com>
2025-02-28 11:18:01 +0100

    finetuning

--- src/index.js
+++ src/index.js
@@ -2,17 +2,20 @@ function generateRecipe(event) {
   event.preventDefault();
   let recipe = document.querySelector("#generatedRecipe");
 
-  recipe.innerHTML = "please wait for the answer";
+  recipe.innerHTML = `please wait for the answer <span class="blink">...<span>`;
 
   let ingredientElement = document.querySelector("#ingredient");
   //let ingredient = ingredientElement.value;
   //console.log(ingredient);
   let apiKey = "1c9131f04b7fo56320ba61f00b43t4cd";
   let context =
-    "You are a great cook. Tell me the name of the Recepie in <strong> with a <br/> after and the display only the main ingredients. please display every ingredient in a separate line with a br/ in between, starting with the first ingredient.";
+    "You are a great cook. Tell me the name of the Recepie in <strong> with a <br/> after and the display only the main ingredients with the quantity. please display every ingredient in a separate line with a br/ in between, starting with the first ingredient.";
   let prompt = `Please give me a short recipe for ${ingredientElement.value} and follow the instructions.`;
 
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let resultElement = document.querySelector("#hidden");
+  resultElement.classList.remove("hidden");
 
   axios.get(apiUrl).then(displayRecipe);
 }
